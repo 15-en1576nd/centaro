e3cff2c6c9d1c86b5a5e79cba723f691ad97d027
Martvdm <martvandermolen@gmail.com>
2022-03-05 14:16:59 +0100

    fix: Create manual record form.

--- app/Http/Controllers/BoardManualRecordController.php
+++ app/Http/Controllers/BoardManualRecordController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use App\Models\board;
 use App\Models\board_manual_record;
@@ -16,10 +17,10 @@ class BoardManualRecordController extends Controller
      */
     public function index()
     {
-       $id = Session::get('currentboardid');
+        $id = Session::get('currentboardid');
         $board = board::where('id', $id)->first(); //Select board  from url-parameter.
 
-         //Split search-result in single array (This was made because many to many result).
+        //Split search-result in single array (This was made because many to many result).
 
         return view('board.records.list', ['board' => $board]);
     }
@@ -37,18 +38,19 @@ class BoardManualRecordController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store($board ,Request $request)
+    public function store($board, Request $request)
     {
         $type = $request->type;
         $value = $request->value;
-        $title =  $request->title;
+        $title = $request->title;
         $discription = $request->discription;
-        if ($type === '-' OR $type === '+' || is_integer($value) || is_string($name)) {
-           $board = board::where('id', $board)->first();
-            board_manual_record::create(array('user_id' => Auth::user()->id,'board_id' => $board->id, 'category_id' => 1,'type' => $type,'value' => $value, 'title' => $title, 'discription' => $discription, 'attachment' => 'test.png'));
+
+        if ($type == "-" || $type == "+" && $value && $title && $discription) {
+            $board = board::where('id', $board)->first();
+            board_manual_record::create(array('user_id' => Auth::user()->id, 'board_id' => $board->id, 'category_id' => 1, 'type' => $type, 'value' => $value, 'title' => $title, 'discription' => $discription, 'attachment' => 'test.png'));
             //FIX!!!!!!!
         }
 
@@ -58,7 +60,7 @@ class BoardManualRecordController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\board_manual_record  $board_manual_record
+     * @param \App\Models\board_manual_record $board_manual_record
      * @return \Illuminate\Http\Response
      */
     public function show(board_manual_record $board_manual_record)
@@ -69,7 +71,7 @@ class BoardManualRecordController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\board_manual_record  $board_manual_record
+     * @param \App\Models\board_manual_record $board_manual_record
      * @return \Illuminate\Http\Response
      */
     public function edit(board_manual_record $board_manual_record)
@@ -80,8 +82,8 @@ class BoardManualRecordController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\board_manual_record  $board_manual_record
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\board_manual_record $board_manual_record
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, board_manual_record $board_manual_record)
@@ -92,7 +94,7 @@ class BoardManualRecordController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\board_manual_record  $board_manual_record
+     * @param \App\Models\board_manual_record $board_manual_record
      * @return \Illuminate\Http\Response
      */
     public function destroy(board_manual_record $board_manual_record)
--- resources/views/board/records/list.blade.php
+++ resources/views/board/records/list.blade.php
@@ -1,9 +1,11 @@
-<a href="/board/{{Session::get('currentboardid')}}"><button>terug</button></a>
+<a href="/board/{{Session::get('currentboardid')}}">
+    <button>terug</button>
+</a>
 <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">
     <form action="/board/{{Session::get('currentboardid')}}/records" method="post">
         @csrf
         <select name="type">
-        <option value="-">-</option>
+            <option value="-">-</option>
             <option value="+">+</option>
         </select>
         <input type="number" name="value" placeholder="Amount. 10">
@@ -18,10 +20,10 @@
 @foreach($board->manual_record->sortByDesc('created_at') as $record)
     <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">
 
-   <div style="width: 100%">{{$record->type . '€' . $record->value}},-</div>
+        <div style="width: 100%">{{$record->type . '€' . $record->value}},-</div>
         <div style="width: 100%">{{$record->title}}</div>
         <div style="width: 100%; flex-wrap: wrap; font-weight: lighter">{{$record->discription}}</div>
         <div style="width: 50%">{{$record->created_at->format('m/d/Y')}}</div>
-</div>
+    </div>
 
 @endforeach
