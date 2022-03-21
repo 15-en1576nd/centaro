fa9bd610886d077058f9d0e7f205cf8feb773f4d
Daneric <danericaalbers@hotmail.com>
2022-03-21 17:21:42 +0100

    feat: added route prefix

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,12 @@ Route::get('/', function () {
 
 Route::get('/account/view', [\App\Http\Controllers\AccountController::class, 'view'])->middleware(['auth'])->name('dashboard');
 
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::resource('boards', \App\Http\Controllers\BoardController::class);
+    Route::resource('boards.records', \App\Http\Controllers\BoardRecordController::class);
+    Route::resource('boards.users', \App\Http\Controllers\BoardUserController::class);
+    Route::resource('boards.category', \App\Http\Controllers\BoardCategoryController::class);
 
-Route::resource('boards', \App\Http\Controllers\BoardController::class);
-Route::resource('boards.records', \App\Http\Controllers\BoardRecordController::class);
-Route::resource('boards.users', \App\Http\Controllers\BoardUserController::class);
-Route::resource('boards.category', \App\Http\Controllers\BoardCategoryController::class);
-
+});
 
 require __DIR__.'/auth.php';
