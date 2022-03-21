4da2ad3818a2ff686e631ae2515df82d2dcd2d0d
Martvdm <martvandermolen@gmail.com>
2022-03-21 12:03:17 +0100

    feat: Saving-target relationships.

--- app/Models/BoardSavingTarget.php
+++ app/Models/BoardSavingTarget.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class BoardSavingTarget extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'board_id',
+        'color_id',
+        'board_user_id',
+        'name',
+        'description',
+        'value',
+        'status',
+        'deadline',
+        ];
+
+    public function board() {
+       return $this->belongsTo(Board::class, 'board_id');
+    }
+    public function color() {
+       return $this->belongsTo(Color::class, 'color_id');
+    }
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
