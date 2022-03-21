da82e23049555d1b06789f683778f81156209714
Martvdm <martvandermolen@gmail.com>
2022-03-21 12:44:51 +0100

    feat: Route group auth middleware

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,17 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/account/view', [\App\Http\Controllers\AccountController::class, 'view'])->middleware(['auth'])->name('dashboard');
+Route::middleware(['web'])->group(function () {
+    Route::get('/account/view', [\App\Http\Controllers\AccountController::class, 'view'])->middleware(['auth'])->name('dashboard');
 
 
-Route::resource('boards', \App\Http\Controllers\BoardController::class);
-Route::resource('boards.records', \App\Http\Controllers\BoardRecordController::class);
-Route::resource('boards.users', \App\Http\Controllers\BoardUserController::class);
-Route::resource('boards.category', \App\Http\Controllers\BoardCategoryController::class);
+    Route::resource('boards', \App\Http\Controllers\BoardController::class);
+    Route::resource('boards.records', \App\Http\Controllers\BoardRecordController::class);
+    Route::resource('boards.users', \App\Http\Controllers\BoardUserController::class);
+    Route::resource('boards.category', \App\Http\Controllers\BoardCategoryController::class);
+});
+
+
 
 
 require __DIR__.'/auth.php';
