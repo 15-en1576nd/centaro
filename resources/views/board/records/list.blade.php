f6e6974870616cc5ec44cf7b45b816c6d02bc4ee
Daneric <danericaalbers@hotmail.com>
2022-03-22 12:06:07 +0100

    feat: transactions sidebar + styling

--- resources/views/board/records/list.blade.php
+++ resources/views/board/records/list.blade.php
@@ -1,25 +1,24 @@
-<a href="/boards/{{$board->id}}">
-    <button>terug</button>
-</a>
-
+@extends('parts.board')
+@section('title', 'Boards')
+@section('content')
 <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">
     <form method="post">
         @csrf
-        <select name="type">
+        <select class="text-black" name="type">
             <option value="-">-</option>
             <option value="+">+</option>
         </select>
-        <select name="category">
+        <select class="text-black" name="category">
             @foreach($board->categories as $category)
                 <option value="{{$category->id}}" style="background: {{$category->color->hexcode}}">{{$category->name}}</option>
             @endforeach
             <option onclick="location.href=('/boards/{{$board->id}}/category')">Create</option>
         </select>
-        <input type="number" step="0.1" name="value" placeholder="Amount. 10">
-        <input type="text" name="title" placeholder="Title">
-        <input type="text" name="description" placeholder="Description">
-        <input type="file" placeholder="pdf, docx, png, jpeg" name="attachment">
-        <input type="submit" value="add">
+        <input class="text-gray-900 placeholder-gray-600" type="number" step="0.1" name="value" placeholder="Amount. 10">
+        <input class="text-gray-900 placeholder-gray-600" type="text" name="title" placeholder="Title">
+        <input class="text-gray-900 placeholder-gray-600" type="text" name="description" placeholder="Description">
+        <input class="text-white placeholder-gray-600" type="file" placeholder="pdf, docx, png, jpeg" name="attachment">
+        <button class="text-white" type="submit">Add</button>
 
     </form>
 </div>
@@ -34,7 +33,7 @@
 @forelse($board->records->sortByDesc('created_at') as $record)
     <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">
 
-        <div style="width: 100%; font-weight: bold; color: @if($record->type == "-")indianred @elseif($record->type == "+") limegreen @endif;">{{$record->type . '€' . $record->value}},-</div>
+        <div style="width: 100%; font-weight: bold;" class="@if($record->type == "-") text-green-500 @elseif($record->type == "+") text-red-500 @endif">{{$record->type . '€' . $record->value}},-</div>
         <div style="width: 100%; color: {{$record->category->color->code}}">{{$record->category->name}}</div>
         <div style="width: 100%; flex-wrap: wrap; font-weight: lighter">{{$record->title}}</div>
         <div style="width: 50%">{{$record->created_at->format('d/m/Y')}}</div>
@@ -45,4 +44,4 @@
         <p>Geen uitgaven/inkomsten gevonden.</p>
     </div>
 @endforelse
-
+@endsection
