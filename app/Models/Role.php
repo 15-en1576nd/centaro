e801d06da1e74888b806bd9b1f081a69618f2a22
Martvdm <martvandermolen@gmail.com>
2022-03-23 11:43:55 +0100

    feat: Pivot table board, user, roles relation

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
-    public function board_users() {
-        return $this->belongsToMany(User::class, 'board_users', 'board_role_id');
+    public function users() {
+        return $this->belongsToMany(User::class, 'board_users', 'role_id');
     }
     public function permissions() {
         return $this->belongsToMany(Permission::class, 'permission_role', 'role_id');
     }
+
 }
---
+++ database/migrations/2022_03_23_103818_create_board_user_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('board_user_roles', function (Blueprint $table) {
+            $table->foreignKey('board_id');
+            $table->foreignKey('user_id');
+            $table->foreignKey('role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('board_user_roles');
+    }
+};
