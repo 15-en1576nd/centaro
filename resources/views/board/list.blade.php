5704928daa129db197caad8c5ec6ac3b522125a0
Daneric <danericaalbers@hotmail.com>
2022-03-23 13:00:14 +0100

    feat: sorted classes

--- resources/views/board/list.blade.php
+++ resources/views/board/list.blade.php
@@ -4,13 +4,13 @@
 <div class="flex p-3 rounded-md justify-centerw-3/4 bg-zinc-900">
     <a class="p-2 transition rounded-md bg-emerald-700 hover:bg-emerald-600" href="/dashboard/boards/create">Create Board</a>
 </div>
-<div class="flex flex-row w-full flex-wrap justify-center">
+<div class="flex flex-row flex-wrap justify-center w-full">
     @forelse(Auth::user()->boards as $board)
-            <div class="flex flex-col m-3 p-2 w-1/4 text-center bg-zinc-900  rounded-md">
+            <div class="flex flex-col w-1/4 p-2 m-3 text-center rounded-md bg-zinc-900">
                 <a href="/dashboard/boards/{{$board->id}}" class="text-3xl break-words">{{$board->name}}: {{$board->id}}</a>
 
                 <p class="capitalize">{{$board->type}}</p>
-                <div class="flex flex-row justify-around flex-wrap">
+                <div class="flex flex-row flex-wrap justify-around">
                     @foreach($board->users as $user)
                         <p style="color: red">{{$user->name}}</p>
                     @endforeach
