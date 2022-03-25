b11d1eb0e813373ec7717355261ac59867b66ada
Martvdm <martvandermolen@gmail.com>
2022-03-25 10:27:58 +0100

    feat: initialize board parts

--- resources/views/board/savingtargets/list.blade.php
+++ resources/views/board/savingtargets/list.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('parts.board')
+@section('title', 'Boards')
+@section('content')
 <a href="/dashboard/boards/{{$board->id}}">
     <button>terug</button>
 </a>
@@ -52,8 +45,4 @@
         <p>Geen spaardoelen gevonden.</p>
     </div>
 @endforelse
-
-
-
-</body>
-</html>
+    @endsection
