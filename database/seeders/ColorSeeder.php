698866e3774918e7bad5fd1dbea079c0855bad5a
Martvdm <martvandermolen@gmail.com>
2022-03-28 11:48:37 +0200

    feat: added more colors to ColorSeeder

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -16,13 +16,18 @@ class ColorSeeder extends Seeder
      */
     public function run()
     {
-        color::create(array('name' => 'indigo', 'code' => '#7114C5', 'class' => 'indigo-600'));
-        color::create(array('name' => 'pink', 'code' => '#fd88f4', 'class' => 'pink-600'));
-        color::create(array('name' => 'seablue', 'code' => '#53b2ff', 'class' => 'blue-600'));
-        color::create(array('name' => 'orange', 'code' => '#ffbb43', 'class' => 'orange-600'));
-        color::create(array('name' => 'green', 'code' => '#38b900', 'class' => 'green-600'));
-        color::create(array('name' => 'limegreen', 'code' => '#32CD32', 'class' => 'limegreen-600'));
-        color::create(array('name' => 'darkblue', 'code' => '#0047AB', 'class' => 'darkblue-600'));
-
+        color::create(array('name' => 'red', 'code' => '#DC2626', 'class' => 'red-600'));
+        color::create(array('name' => 'lightred', 'code' => '#F87171', 'class' => 'red-400'));
+        color::create(array('name' => 'orange', 'code' => '#F97316', 'class' => 'orange-500'));
+        color::create(array('name' => 'yellow', 'code' => '#FDE047', 'class' => 'yellow-300'));
+        color::create(array('name' => 'gold', 'code' => '#CA8A04', 'class' => 'yellow-600'));
+        color::create(array('name' => 'limegreen', 'code' => '#84CC16', 'class' => 'lime-500'));
+        color::create(array('name' => 'lightgreen', 'code' => '#4ADE80', 'class' => 'green-400'));
+        color::create(array('name' => 'darkgreen', 'code' => '#15803D', 'class' => 'green-700'));
+        color::create(array('name' => 'cyan', 'code' => '#0D9488', 'class' => 'teal-600'));
+        color::create(array('name' => 'blue', 'code' => '#2563eb', 'class' => 'blue-600'));
+        color::create(array('name' => 'lightblue', 'code' => '#38bdf8', 'class' => 'sky-400'));
+        color::create(array('name' => 'purple', 'code' => '#7c4aed', 'class' => 'violet-600'));
+        color::create(array('name' => 'pink', 'code' => '#a855f7', 'class' => 'purple-500'));
     }
 }
