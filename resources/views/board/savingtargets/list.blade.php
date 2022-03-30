d2b93a8006939839a1cf6593d59b56863f636aac
Martvdm <martvandermolen@gmail.com>
2022-03-30 12:58:01 +0200

    feat: adaptive progress bar

--- resources/views/board/savingtargets/list.blade.php
+++ resources/views/board/savingtargets/list.blade.php
@@ -25,7 +25,8 @@
     <br><br>
     <div class="flex flex-wrap flex-row justify-center">
         @forelse($board->savingtargets as $savingtarget)
-            <div class="w-1/4 flex-wrap m-3 p-4 text-gray-900 bg-white border-l-4 border-{{$savingtarget->color->class}} rounded-lg">
+            <div
+                class="w-1/4 flex-wrap m-3 p-4 text-gray-900 bg-white border-l-4 border-{{$savingtarget->color->class}} rounded-lg">
                 <div class="flex items-center">
                     <div class="icon w-14 p-3.5 bg-{{$savingtarget->color->class}} text-white rounded-full mr-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
@@ -34,7 +35,7 @@
                                   d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"/>
                         </svg>
                     </div>
-                    <div class="flex flex-col justify-center">
+                    <div class="flex flex-col justify-center w-full">
                         <div class="text-lg">{{$savingtarget->name}}</div>
                         <div class="text-sm text-emerald-500">Goal: €{{$savingtarget->value}}</div>
                         <div class="text-sm text-red-500">To go: @if($savingtarget->value > $total)
@@ -46,16 +47,28 @@
 
                         <div
                             class="text-sm text-gray-500">@if((new DateTime()) < (new DateTime($savingtarget->deadline)))
-                                Finishes over: {{$daycount = (new DateTime())->diff(new DateTime($savingtarget->deadline))->days}} @if($daycount > 1 )days @else day @endif
+                                Finishes
+                                over: {{$daycount = (new DateTime())->diff(new DateTime($savingtarget->deadline))->days}} @if($daycount > 1 )
+                                    days @else day @endif
                             @elseif((new DateTime())->diff(new DateTime($savingtarget->deadline))->days = 0 )
                                 The deadline has passed today!
-                             @else
+                            @else
 
-                                The deadline passed {{$daycount = (new DateTime())->diff(new DateTime($savingtarget->deadline))->days}} @if($daycount > 1 )days @else day @endif ago!
+                                The deadline
+                                passed {{$daycount = (new DateTime())->diff(new DateTime($savingtarget->deadline))->days}} @if($daycount > 1 )
+                                    days @else day @endif ago!
                             @endif
-                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-                                <div class="bg-{{$savingtarget->color->class}} text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+
+                        </div>
+                        <div class="relative flex align-bottom w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                            <div
+                                style="width: @if($total < $savingtarget->value){{$progress = floor($total / $savingtarget->value * 100)}}@else{{$progress = 100}}@endif%"
+                                class="absolute bg-blue-600 text-xs font-medium text-blue-100 text-center p-3 leading-none rounded-full"
+                                >
+
                             </div>
+                            <div class="z-10 relative w-full outline outline-white text-center">{{$progress}}%</div>
+
                         </div>
                     </div>
 
