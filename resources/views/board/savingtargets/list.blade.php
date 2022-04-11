9aa2094ba1a15b27ef38ad26acca046c6435e2e0
Martvdm <martvandermolen@gmail.com>
2022-04-11 11:23:20 +0200

    feat: Savingtarget card

--- resources/views/board/savingtargets/list.blade.php
+++ resources/views/board/savingtargets/list.blade.php
@@ -41,8 +41,20 @@
                         </svg>
                     </div>
                     <div class="flex flex-col justify-center w-full">
-                        <div class="text-lg hover:underline cursor-pointer">
-                            <a href="{{ route('boards.savingtargets.show', ['board' => $board, 'boardsavingtarget' => $savingtarget->id])}}">{{$savingtarget->name}}</a>
+                        <div class="text-lg justify-between flex flex-row">
+                            <a class="hover:underline cursor-pointer" href="{{ route('boards.savingtargets.show', ['board' => $board, 'boardsavingtarget' => $savingtarget->id])}}">{{$savingtarget->name}}</a>
+                           <div class="flex flex-row space-x-1">
+                            <a href="/boards/{{$board->id}}/savingtargets/{{$savingtarget->id}}/edit">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 flex justify-end hover:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                            </svg>
+                            </a>
+                            <a href="" class="cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg>
+                            </a>
+                           </div>
                         </div>
                         <div class="text-sm text-emerald-500">Goal: €{{$savingtarget->value}}</div>
                         <div class="text-sm text-red-500">To go: @if($savingtarget->value > $total)
