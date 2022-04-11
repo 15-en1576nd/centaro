b6087c9defd07df3bd28fc518fe36aa76dab6393
Martvdm <martvandermolen@gmail.com>
2022-04-11 11:22:55 +0200

    feat: Changes header savingtargets

--- resources/views/board/savingtargets/list.blade.php
+++ resources/views/board/savingtargets/list.blade.php
@@ -1,32 +1,37 @@
 @extends('parts.board')
 @section('title', 'Boards')
 @section('content')
-
-    <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">
-        <form method="post">
-
-            @csrf
-
-
-            <input class="text-gray-900 placeholder-gray-600" type="number" step="0.1" name="value"
-                   placeholder="Amount. 10">
-            <input class="text-gray-900 placeholder-gray-600" type="text" name="title" placeholder="Title">
-            <input class="text-gray-900 placeholder-gray-600" type="text" name="description" placeholder="Description">
-            <input class="text-gray-900 placeholder-gray-600" type="date" name="deadline">
-            <select class="text-gray-900 placeholder-gray-600" name="color">
-                @foreach($colors as $color)
-                    <option value="{{$color->id}}" style="background: {{$color->code}}">{{$color->name}}</option>
-                @endforeach
-            </select>
-            <input type="submit" value="add">
-
-        </form>
+    <div class="flex p-3 rounded-md justify-between bg-zinc-900 w-full text-center">
+        <a class="flex p-2 transition rounded-md bg-emerald-700 hover:bg-emerald-600"
+           href="/dashboard/boards/{{$board->id}}/savingtargets">Back</a>
+        <div class="text-3xl text-white w-full justify-center flex flex-nowrap space-x-4">
+            Savingtargets</div>
+        <a class="flex p-2 transition rounded-md bg-emerald-700 hover:bg-emerald-600"
+           href="/dashboard/boards/{{$board->id}}/savingtargets/create">Create</a>
+        </div>
     </div>
+{{--    <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">--}}
+{{--        <form method="post">--}}
+{{--            @csrf--}}
+{{--            <input class="text-gray-900 placeholder-gray-600" type="number" step="0.1" name="value"--}}
+{{--                   placeholder="Amount. 10">--}}
+{{--            <input class="text-gray-900 placeholder-gray-600" type="text" name="title" placeholder="Title">--}}
+{{--            <input class="text-gray-900 placeholder-gray-600" type="text" name="description" placeholder="Description">--}}
+{{--            <input class="text-gray-900 placeholder-gray-600" type="date" name="deadline">--}}
+{{--            <select class="text-gray-900 placeholder-gray-600" name="color">--}}
+{{--                @foreach($colors as $color)--}}
+{{--                    <option value="{{$color->id}}" style="background: {{$color->code}}">{{$color->name}}</option>--}}
+{{--                @endforeach--}}
+{{--            </select>--}}
+{{--            <input type="submit" value="add">--}}
+
+{{--        </form>--}}
+{{--    </div>--}}
     <br><br>
     <div class="flex flex-wrap flex-row justify-center">
         @forelse($board->savingtargets as $savingtarget)
             <div
-                class="w-1/4 flex-wrap m-3 p-4 text-gray-900 bg-white border-l-4 border-{{$savingtarget->color->class}} rounded-lg">
+                class="w-1/4 flex-wrap m-3 p-4 text-white bg-zinc-900 border-l-4 border-{{$savingtarget->color->class}} rounded-lg">
                 <div class="flex items-center">
                     <div class="icon w-14 p-3.5 bg-{{$savingtarget->color->class}} text-white rounded-full mr-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
@@ -61,15 +66,13 @@
                                     days @else day @endif ago!
                             @endif
                         </div>
-                        <div class="relative flex bottom w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                        <div class="relative flex bottom w-full bg-gray-900 rounded-full dark:bg-gray-200">
                             <div
                                 style="width: @if($total < $savingtarget->value){{$progress = floor($total / $savingtarget->value * 100)}}@else{{$progress = 100}}@endif%"
                                 class="absolute bg-blue-600 text-xs font-medium text-blue-100 text-center p-3 leading-none rounded-full"
                             >
-
                             </div>
-                            <div class="z-10 relative w-full outline outline-white text-center">{{$progress}}%</div>
-
+                            <div class="z-10 relative w-full outline-none text-center">{{$progress}}%</div>
                         </div>
                     </div>
 
