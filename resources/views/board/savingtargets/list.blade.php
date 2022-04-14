d5e46078ffdf8058a6535b2fa0b880213da62b8c
Martvdm <martvandermolen@gmail.com>
2022-04-14 11:50:39 +0200

    feat: More information list blade

--- resources/views/board/savingtargets/list.blade.php
+++ resources/views/board/savingtargets/list.blade.php
@@ -1,35 +1,23 @@
 @extends('parts.board')
 @section('title', 'Boards')
 @section('content')
-    <div class="flex p-3 rounded-md justify-between bg-zinc-900 w-full text-center">
+    <div class="flex p-3 rounded-md justify-between bg-zinc-900 w-full text-center flex-col">
+        <div class="flex flex-row">
         <a class="flex p-2 transition rounded-md bg-emerald-700 hover:bg-emerald-600"
-           href="/dashboard/boards/{{$board->id}}/savingtargets">Back</a>
+           href="/dashboard/boards/{{$board->id}}/">Back</a>
         <div class="text-3xl text-white w-full justify-center flex flex-nowrap space-x-4">
             Savingtargets</div>
         <a class="flex p-2 transition rounded-md bg-emerald-700 hover:bg-emerald-600"
            href="/dashboard/boards/{{$board->id}}/savingtargets/create">Create</a>
         </div>
+        <div class="flex flex-row justify-between pt-2 text-gray-400">
+            <p>Total savingtargets inactive: {{count($board->savingtargets->where('status', '=', 'inactive'))}}</p>
+            <p>Total savingtargets active: {{count($board->savingtargets->where('status', '=', 'active'))}}</p>
+            <p>Total savingtargets finished: {{count($board->savingtargets->where('status', '=', 'finished'))}}</p>
+        </div>
     </div>
-{{--    <div style="border: 0.5px black solid; padding: 2px; display: flex; flex-direction: row">--}}
-{{--        <form method="post">--}}
-{{--            @csrf--}}
-{{--            <input class="text-gray-900 placeholder-gray-600" type="number" step="0.1" name="value"--}}
-{{--                   placeholder="Amount. 10">--}}
-{{--            <input class="text-gray-900 placeholder-gray-600" type="text" name="title" placeholder="Title">--}}
-{{--            <input class="text-gray-900 placeholder-gray-600" type="text" name="description" placeholder="Description">--}}
-{{--            <input class="text-gray-900 placeholder-gray-600" type="date" name="deadline">--}}
-{{--            <select class="text-gray-900 placeholder-gray-600" name="color">--}}
-{{--                @foreach($colors as $color)--}}
-{{--                    <option value="{{$color->id}}" style="background: {{$color->code}}">{{$color->name}}</option>--}}
-{{--                @endforeach--}}
-{{--            </select>--}}
-{{--            <input type="submit" value="add">--}}
-
-{{--        </form>--}}
-{{--    </div>--}}
-    <br><br>
     <div class="flex flex-wrap flex-row justify-center">
-        @forelse($board->savingtargets as $savingtarget)
+        @forelse($board->savingtargets->sortByDesc('created_at') as $savingtarget)
             <div
                 class="w-1/4 flex-wrap m-3 p-4 text-white bg-zinc-900 border-l-4 border-emerald-700 rounded-lg">
                 <div class="flex items-center">
@@ -41,14 +29,19 @@
                     </div>
                     <div class="flex flex-col justify-center w-full">
                         <div class="text-lg justify-between flex flex-row">
-                            <a class="hover:underline cursor-pointer" href="{{ route('boards.savingtargets.show', ['board' => $board, 'boardsavingtarget' => $savingtarget->id])}}">{{$savingtarget->name}}</a>
+                            <a class="hover:underline cursor-pointer flex flex-row items-center" href="{{ route('boards.savingtargets.show', ['board' => $board, 'boardsavingtarget' => $savingtarget->id])}}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                </svg>
+                                {{$savingtarget->name}}</a>
                            <div class="flex flex-row space-x-1">
                             <a href="/boards/{{$board->id}}/savingtargets/{{$savingtarget->id}}/edit">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 flex justify-end hover:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                             </svg>
                             </a>
-                            <a href="" class="cursor-pointer">
+                            <a class="cursor-pointer" data-modal-toggle="popup-modal">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                 </svg>
@@ -106,14 +99,21 @@
                                     days @else day @endif ago!
                             @endif
                         </div>
+                        <div class="flex flex-row">
                         <div class="relative flex bottom w-full bg-gray-900 rounded-full dark:bg-gray-200">
                             <div
                                 style="width: @if($total < $savingtarget->value){{$progress = floor($total / $savingtarget->value * 100)}}@else{{$progress = 100}}@endif%"
-                                class="absolute bg-blue-600 text-xs font-medium text-blue-100 text-center p-3 leading-none rounded-full"
+                                class="absolute bg-blue-600 text-xs font-medium text-blue-100 text-center p-3 px-0 leading-none rounded-full"
                             >
                             </div>
                             <div class="z-10 relative w-full outline-none text-center">{{$progress}}%</div>
                         </div>
+                        @if($progress == 100)
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-6 w-6 rounded-md hover:bg-emerald-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                            </svg>
+                        @endif
+                    </div>
                     </div>
 
                 </div>
@@ -127,4 +127,5 @@
             </div>
         @endforelse
     </div>
+    <script src="//unpkg.com/alpinejs" defer></script>
 @endsection
