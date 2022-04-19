dd666fd7e7c4ecd005350de1940f1c21b3f5f617
Martvdm <martvandermolen@gmail.com>
2022-04-19 17:05:35 +0200

    feat: create savingtarget blade

--- resources/views/board/savingtargets/create.blade.php
+++ resources/views/board/savingtargets/create.blade.php
@@ -1,6 +1,7 @@
 @extends('parts.board')
 @section('title', 'Boards')
 @section('content')
+
     <div class="flex p-3 rounded-md justify-center-3/4 bg-zinc-900">
         <a class="p-2 transition rounded-md bg-emerald-700 hover:bg-emerald-600" href="/dashboard/boards/{{$board->id}}/savingtargets">Back</a>
         <div class="text-3xl text-white w-full justify-center flex flex-nowrap space-x-4">
@@ -55,13 +56,13 @@
                 Source base
                 <div class="m-7 text-gray-400 text-base">
                         <p class="font-bold flex flex-row">
-                            <input type="radio" name="source" value="manual" class="p-2 mr-2 rounded-md bg-zinc-700 text-white">
+                            <input type="radio" value="manual" name="type" class="p-2 mr-2 rounded-md bg-zinc-700 text-white" checked>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
                             </svg>
                             Manual: </p>You can add the value to the savingtarget manually.<p class="text-sm text-gray-600">[Available in percentages]</p>
                         <p class="font-bold flex flex-row">
-                            <input type="radio" name="source" value="auto" class="p-2 mr-2 rounded-md bg-zinc-700 text-white">
+                            <input type="radio" value="auto" name="type" class="p-2 mr-2 rounded-md bg-zinc-700 text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -69,9 +70,45 @@
                             Automatic: </p>The saving target value is based on the total of the board.
                     <p class="text-sm text-gray-600">[Available in percentages]</p>
                 </div>
+
+                <div class="percentage text-xl">
+                    <label for="value" class="text-white">Percentage (Optional)</label>
+                    <input type="range" name="amountRange" min="0" max="100" value="100" oninput="this.form.amountInput.value=this.value" />
+                    <div class="flex-row">
+                    <input type="number" style="-webkit-appearance: none;" class="appearance-none h-6 w-20 rounded-md bg-zinc-700 text-white" name="amountInput" min="0" max="100" value="0" oninput="this.form.amountRange.value=this.value" />
+                    %
+                    </div>
+                    <a onclick="CategoryToggle()" class="text-blue-600 text-sm cursor-pointer underline">Base on category...</a>
+                </div>
+                <div class="category text-xl">
+                <select class="rounded-non w-full flex flex-row h-15 overflow-y-scroll flex-wrap bg-zinc-900" size="3" name="category[]" multiple>
+                    @foreach($board->categories as $category)
+                        <option style="background: {{$category->color->code}}" value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
             </div>
+            </div>
+
         </div>
     </div>
+        <script>
+            $(document).ready(function(){
+                $(".category").hide();
+                $(".percentage").hide();
+                $('input[type="radio"][name="type"]').click(function(){
+                    if($(this).val() == 'manual'){
+                        $(".percentage").hide();
+                    }
+                    else if($(this).val() == 'auto'){
+                        $(".percentage").show();
+                    }
+                });
+            });
+            function CategoryToggle() {
+                var x = document.getElementsByClassName("category");
+                $(".category").toggle();
+            }
+        </script>
     <div class="flex mt-6 p-3 rounded-md justify-center bg-zinc-900">
         <input type="submit" value="Create" class="p-2 rounded-md bg-emerald-700 hover:bg-emerald-600">
     </div>
