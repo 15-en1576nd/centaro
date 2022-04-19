4bee17131c5152cf73bab3c823d814967e7e2739
Daneric <danericaalbers@hotmail.com>
2022-04-19 22:19:18 +0200

    feat: dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,38 +1,6 @@
 @extends('parts.main')
 @section('title', 'Dashboard')
 @section('content')
-<h1 class="font-bold text-xl">Welcome {{ Auth::user()->name . ' ' . Auth::user()->surname }}</h1>
-<div class="flex flex-col">
-    <p>Your unique user id: <strong>{{Auth::user()->id}}</strong></p>
-    <p>First Name: {{Auth::user()->name}}</p>
-    <p>Surname: {{Auth::user()->surname}}</p>
-    <p>Email: {{Auth::user()->email}}</p>
-</div>
-@if(isset(Auth::user()->preference))
-    Lang:
-{{Auth::user()->preference->lang}}
-    Valuta:
-{{Auth::user()->preference->valuta}}
-@endif
-
-<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><button>Logout</button></a>
-
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
-<a href="/boards/create"><button>Create</button></a> Boards:
-
-@forelse(Auth::user()->boards as $board)
-    <a style="text-decoration: none" href="/boards/{{$board->id}}">
-    <div style="background: #2563eb; color:white; border: 1px solid black; padding: 2px; display: flex; flex-direction: column; width: max-content; height: auto">
-        <h3>{{$board->name}}: {{$board->id}}</h3>
-      <p>{{$board->type}}</p>
-        @foreach($board->users as $user)
-           <p style="color: red">{{$user->name}}</p>
-        @endforeach
-    </div>
-    </a>
-@empty
-    <p style="color: gray">Geen borden gevonden!</p>
-@endforelse
+<h1 class="text-3xl font-bold text-center">Welcome {{ Auth::user()->name . ' ' . Auth::user()->surname }}</h1>
+<p class="mt-3 text-lg">Welcome to Centaro, centaro is a place where you can manage your transactions, saving goals and alot more!</p>
 @endsection
