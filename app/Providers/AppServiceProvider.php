c2934f88fb9f4d7ebc308bf553d9d94674c681e7
Martvdm <martvandermolen@gmail.com>
2022-04-21 11:16:28 +0200

    feat: Added charts to board home

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,21 @@
 
 namespace App\Providers;
 
+use ConsoleTVs\Charts\Registrar as Charts;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+    // ...
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(Charts $charts)
     {
-        //
+        $charts->register([
+            \App\Charts\CategoryChart::class,
+            \App\Charts\CategoryNegativeChart::class
+        ]);
     }
 }
